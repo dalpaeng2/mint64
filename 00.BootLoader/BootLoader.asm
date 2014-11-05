b7b75fc0dceb29cab0e3db3a63a583269635bd17
dalpaeng2 <dalpaeng2@gmail.com>
2014-11-05 14:41:57 +0900

    Add Booting Message.

--- 00.BootLoader/BootLoader.asm
+++ 00.BootLoader/BootLoader.asm
@@ -3,13 +3,52 @@
 
 SECTION .text	                ; text 섹션(세그먼트)을 정의
 
-mov ax, 0xB800                ; AX 레지스터에 0xB8800 복사
-mov ds, ax                    ; DS 세그먼트 레지스터에 AX 레지스터의 값(0xB800)을 복사
+jmp 0x07C0:START              ; CS 세그먼트 레지스터에 0x07C0을 복사하면서, START 레이블로 이동
 
-mov byte [0x00], 'M'          ; DS 세그먼트:오프셋 0xB800:0x0000에 "M"을 복사
-mov byte [0x01], 0x4A         ; DS 세그먼틔:오프셋 0xB800:0x0001에 0x4A를 복사
+START:
+    mov ax, 0x07C0            ; 부트로터의 시작 어드레스(0x7C00)를 세그먼트 레지스터 값으로 변환
+    mov ds, ax                ; DS 세그먼트 레지스터에 설정
+    mov ax, 0xB800            ; 비디오 메모리의 시작 어드레스(0xB800)를 세그먼트 레지스터 값으로 변환
+    mov es, ax                ; ES 세그먼트 레지스터에 설정
 
-jmp $				                 	; 현재 위치에서 무한 루프 수행
+    mov si, 0                 ; SI 레지스터(문자열 원본 인덱스 레지스터)를 초기화
+
+
+.SCREENCLEARLOOP:
+    mov byte [ es: si ], 0        ; 비디오 메모리의 문자가 위치하는 어드레스에
+		                              ; 0을 복사하여 문자를 삭제
+    mov byte [ es: si + 1 ], 0x0A ; 비디오 메모리의 속성이 위치하는 어드레스에
+                                  ; 0x0A을 복사
+
+    add si, 2                     ; 다음 문자 위치
+
+    cmp si, 80 * 25 * 2           ; 화면의 전체 크기와 비교
+
+    jl .SCREENCLEARLOOP           ; SI < (80 * 25 * 2)이면 .SCREENCLEARLOOP로 이동
+
+    mov si, 0                     ; SI 레지스터(문자열 원본 인덱스 레지스터) 초기화
+    mov di, 0                     ; DI 레지스터(문자열 대상 인덱스 레지스터) 초기화
+
+
+.MESSAGELOOP:
+    mov cl, byte [ si + MESSAGE1 ]    ; MESSAGE1의 어드레스에서 SI 레지스터 값만큼
+                                      ; 더한 위치의 문자를 CL 레지스터에 복사
+
+    cmp cl, 0                         ; 복사한 문자와 0을 비교
+    je .MESSAGEEND                    ; 복사한 문자가 0이면 .MESSAGEEND로 이동
+
+    mov byte [ es: di ], cl           ; 0이 아니면 비디오 메모리 어드레스 0xB800:di에 문자를 출력
+
+    add si, 1                         ; 다음 문자열
+    add di, 2                         ; 비디오 메모리는 (문자, 속성) 2 바이트 이므로 2를 더함
+
+    jmp .MESSAGELOOP                  ; 메시지 출력 루프 반복
+.MESSAGEEND:
+
+    jmp $				                 	    ; 현재 위치에서 무한 루프 수행
+
+MESSAGE1:    db 'MINT64 OS Boot Loader Start', 0        ; 출력할 메시지 정의
+
 
 times 510 - ($ - $$) db 0x00  ; $: 현재 라인의 어드레스
                               ; $$: 현재 섹션(.text)의 시작 어드레스
