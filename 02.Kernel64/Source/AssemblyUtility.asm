fb728d76161ffe555695c537de7f336e4b98acf5
dalpaeng2 <dalpaeng2@gmail.com>
2014-11-15 22:07:21 +0900

    Add a Task Structure and Task Switching.

--- 02.Kernel64/Source/AssemblyUtility.asm
+++ 02.Kernel64/Source/AssemblyUtility.asm
@@ -5,6 +5,7 @@ SECTION .text
 global kInPortByte, kOutPortByte, kLoadGDTR, kLoadTR, kLoadIDTR
 global kEnableInterrupt, kDisableInterrupt, kReadRFLAGS
 global kReadTSC
+global kSwitchContext
 
 kInPortByte:
   push rdx
@@ -75,3 +76,113 @@ kReadTSC:
   pop  rdx
 
   ret
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; 태스크 관련 어셈블리어 함수
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+%macro KSAVECONTEXT 0
+  push rbp
+  push rax
+  push rbx
+  push rcx
+  push rdx
+  push rdi
+  push rsi
+  push r8
+  push r9
+  push r10
+  push r11
+  push r12
+  push r13
+  push r14
+  push r15
+
+  mov  ax, ds
+  push rax
+  mov  ax, es
+  push rax
+  push fs
+  push gs
+%endmacro
+
+%macro KLOADCONTEXT 0
+  pop  gs
+  pop  fs
+  pop  rax
+  mov  es, ax
+  pop  rax
+  mov  ds, ax
+
+  pop  r15
+  pop  r14
+  pop  r13
+  pop  r12
+  pop  r11
+  pop  r10
+  pop  r9
+  pop  r8
+  pop  rsi
+  pop  rdi
+  pop  rdx
+  pop  rcx
+  pop  rbx
+  pop  rax
+  pop  rbp
+%endmacro
+
+; Current Context에 현재 콘택스트를 저장하고 Next Task에서 콘텍스트를 복구
+;   PARAM: Current Context, Next Context
+kSwitchContext:
+  push rbp
+  mov  rbp, rsp
+
+  ; Current Context 가 NULL이면 콘텍스트를 저장할 필요 없음
+  pushfq
+  cmp  rdi, 0
+  je   .LoadContext
+  popfq
+
+  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+  ; 현재 태스크의 콘텍스트를 저장
+  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+  push rax
+
+  mov  ax, ss                               ; SS 레지스터 저장
+  mov  qword[ rdi + ( 23 * 8 ) ], rax
+
+  mov  rax, rbp                             ; RBP에 저장된 RSP 레지스터 저장
+  add  rax, 16                              ; RSP 레지스터는 push rbp와 Return Address를
+  mov  qword[ rdi + ( 22 * 8 ) ], rax       ; 제외한 값으로 저장
+
+  pushfq                                    ; RFLAGS 저장
+  pop  rax
+  mov  qword[ rdi + ( 21 * 8 ) ], rax
+
+  mov  ax, cs                               ; CS 레지스터 저장
+  mov  qword[ rdi + ( 20 * 8 ) ], rax
+
+  mov  rax, qword[ rbp + 8 ]                ; RIP 레지스터를 Return Address로 설정하여
+  mov  qword[ rdi + ( 19 * 8 ) ], rax       ; 다음 콘텍스트 복원 시에 이 함수를 호출한
+                                            ; 위치로 이동하게 함
+
+  pop  rax
+  pop  rbp
+
+  ; 가장 끝부분에 SS, RSP, RFLAGS, CS, RIP 레지스터를 저장했으므로, 이전 영역에
+  ; push 명령어로 콘텍스트를 저장하기 위해 스택을 변경
+  add  rdi, ( 19 * 8 )
+  mov  rsp, rdi
+  sub  rdi, ( 19 * 8 )
+
+  ; 나머지 레지스터를 모두 Context 자료구조에 저장
+  KSAVECONTEXT
+
+  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+  ; 다음 태스크의 콘텍스트 복원
+  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+.LoadContext:
+  mov  rsp, rsi
+
+  ; Context 자료구조에서 레지스터를 복원
+  KLOADCONTEXT
+  iretq
